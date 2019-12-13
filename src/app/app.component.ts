6c16e9136176848d7fa20544f2ad65ce1c5b139c
James <thenetimp@gmail.com>
2019-12-14 07:46:27 +0900

    files for sending to lambda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'ssr-serverless';
+  platform: string;
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+  public ngOnInit(): void {
+    this.platform = isPlatformBrowser(this.platformId) ? 'Browser' : 'Server';
+  }
 }
